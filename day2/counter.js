718e3d37679b1dc14022a9d84404de65f87b53a0
luk1790 <luk1790@gmail.com>
2020-12-02 16:16:56 +0100

    day2

--- day2/counter.js
+++ day2/counter.js
@@ -2,28 +2,38 @@ var data = require("./data");
 var pass = data.data();
 
 let counter = 0;
+let counterSpec = 0;
 
-
-let mapPassword = pass.map(val => {
-  let data = val.split(' ');
-  let numbers = data[0].split('-');
+let mapPassword = pass.map((val) => {
+  let data = val.split(" ");
+  let numbers = data[0].split("-");
   return {
     numberMin: numbers[0],
     numberMax: numbers[1],
-    letter:  data[1].replace(':', ''),
-    password: data[2]
+    letter: data[1].replace(":", ""),
+    password: data[2],
+  };
+});
+
+mapPassword.forEach((val) => {
+  // console.log("password: %o, letter: %o", val.password, val.letter);
+  let stringMatch = new RegExp(val.letter, "g");
+  let letterCount = (val.password.match(stringMatch) || []).length;
+  // console.log(letterCount);
+  if (letterCount <= val.numberMax && letterCount >= val.numberMin) {
+    counter++;
+  }
+
+  if (
+    (val.password[val.numberMin-1] === val.letter &&
+      val.password[val.numberMax-1] !== val.letter) ||
+    (val.password[val.numberMin-1] !== val.letter &&
+      val.password[val.numberMax-1] === val.letter)
+  ) {
+    console.log("password: %o, letter: %o, position %o - %o", val.password, val.letter, val.numberMin, val.numberMax);
+    counterSpec++
   }
 });
 
-mapPassword.forEach(val => {
-  console.log('password: %o, letter: %o',val.password, val.letter);
-  let stringMatch = new RegExp(val.letter, "g");
-  let letterCount = (val.password.match(stringMatch) || []).length;
-  console.log(letterCount);
-  if(letterCount <= val.numberMax && letterCount >= val.numberMin){
-    counter++;
-  }
-})
-
-console.log('result: %o', counter)
-
+console.log("result: %o", counter);
+console.log("result 2: %o", counterSpec);
