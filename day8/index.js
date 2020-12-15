5b44463c3faf2f45a1d3b89eb3836c4d7694f672
luk1790 <luk1790@gmail.com>
2020-12-15 17:49:02 +0100

    format

--- day8/index.js
+++ day8/index.js
@@ -30,17 +30,23 @@ var downloadFile = (fileName) => {
     let actualCommand = localData[0];
     let counter = 0;
     let acc = 0;
-let oneChange = true;
-    while (counter < localData.length && actualCommand.count === 0 ) {
+    let oneChange = true;
+    while (counter < localData.length && actualCommand.count === 0) {
       // console.log("%o - %o  %o", actualCommand, counter, localData.length);
       if (actualCommand.command === "acc") {
         acc += actualCommand.number;
         localData[counter].count++;
         counter++;
       } else if (actualCommand.command === "jmp") {
-        console.log("%o - %o  %o %o", actualCommand, counter,counter + 1, localData.length);
+        console.log(
+          "%o - %o  %o %o",
+          actualCommand,
+          counter,
+          counter + 1,
+          localData.length
+        );
         if (counter + 1 === localData.length - 1 && oneChange) {
-          console.log('sd')
+          console.log("sd");
           localData[counter].count++;
           counter++;
           oneChange = false;
@@ -49,12 +55,21 @@ let oneChange = true;
           counter += actualCommand.number;
         }
       } else if (actualCommand.command === "nop") {
-        console.log("%o - %o  %o %o", actualCommand, counter, counter + actualCommand.number, localData.length);
-        if (counter + actualCommand.number === localData.length - 1 && oneChange) {
-          console.log('sd')
+        console.log(
+          "%o - %o  %o %o",
+          actualCommand,
+          counter,
+          counter + actualCommand.number,
+          localData.length
+        );
+        if (
+          counter + actualCommand.number === localData.length - 1 &&
+          oneChange
+        ) {
+          console.log("sd");
           localData[counter].count++;
           counter += actualCommand.number;
-          oneChange = false
+          oneChange = false;
         } else {
           localData[counter].count++;
           counter++;
